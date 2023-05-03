5028e5dda10dbb6e1172921df926092cfd2fdf91
jeonga-Son <jjeong0106@gmail.com>
2023-05-03 19:16:32 +0900

    URL : queryString

--- WEB2_Nodejs/syntax/Template.js
+++ WEB2_Nodejs/syntax/Template.js
@@ -4,6 +4,8 @@ var letter = `Dear ${str} Lorem Ipsum
 
 
 
-is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, ${str} when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, ${str} remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. ${str}`;
+is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, ${str} when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, ${
+  1 + 1
+} remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. ${str}`;
 
 console.log(letter);
